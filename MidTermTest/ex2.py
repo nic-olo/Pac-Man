dbcf5f22727c52957f60618fc29caab48cf9b9d6
nicolo <nicolo.micheletti@student manchester.ac.uk>
2021-11-09 10:16:10 +0000

    ex2

--- MidTermTest/ex2.py
+++ MidTermTest/ex2.py
@@ -1,4 +1,4 @@
-with open('test_file3.txt', 'r') as file:
+with open('test_file2.txt', 'r') as file:
     line = file.read()
 
 line = list(line)
@@ -6,15 +6,35 @@ c = ''
 
 for i in range(20):
     if line[i] == ':':
-        c = line[i-1]
-        del line[:i+1]
+        c = line[i - 1]
+        del line[:i + 1]
         break
 
 if c == 'x':
     print('hexadecimal')
 
 elif c == ')':
-    print('caesar')
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+                'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+    decoder = ['x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+               'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    message = []
+    words = ''.join(line)
+    words = words.split(' ')
+    words[-1] += ' '
+    for word in words:
+        startIndex = 0
+        letters = []
+        for i in range(len(word)):
+            if word[i] == ' ':
+                continue
+            else:
+                letters.append(decoder[alphabet.index(word[i])])
+        message.append(''.join(letters))
+
+    print(' '.join(message))
+
 
 elif c == 'e':
     decoder = {'.-': 'a', '-...': 'b', '-.-.': 'c',
@@ -23,7 +43,7 @@ elif c == 'e':
                '.-..': 'l', '--': 'm', '-.': 'n', '---': 'o', '.--.': 'p', '--.-': 'q',
                '.-.': 'r', '...': 's', '-': 't', '..-': 'u', '...-': 'v',
                '.--': 'w', '-..-': 'x', '-.--': 'y', '--..': 'z'
-    }
+               }
     message = []
     words = ''.join(line)
     words = words.split('/ ')
@@ -34,8 +54,7 @@ elif c == 'e':
         for i in range(len(word)):
             if word[i] == ' ':
                 letters.append(decoder[word[startIndex:i]])
-                startIndex = i+1
+                startIndex = i + 1
         message.append(''.join(letters))
 
     print(' '.join(message))
-
